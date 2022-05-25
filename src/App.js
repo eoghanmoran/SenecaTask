1706e482c35829528a488732f622ed217a31346d
40203858 <emoran05@qub.ac.uk>
2022-05-25 23:36:34 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -4,16 +4,61 @@ import "./styles.css";
 import SwitchSelector from "react-switch-selector";
 
 
-const options = [
+const options1 = [
   {
-    label: <span>Tiger Woods</span>,
+    label: <span>Cell Wall</span>,
     value: {
       foo: true
     },
     selectedBackgroundColor: "#0097e6",
   },
   {
-    label: "Jack Nicklaus",
+    label: "Ribosomes",
+    value: "bar",
+    selectedBackgroundColor: "#fbc531"
+  }
+];
+
+const options2 = [
+  {
+    label: <span>Cytoplasm</span>,
+    value: {
+      foo: true
+    },
+    selectedBackgroundColor: "#0097e6",
+  },
+  {
+    label: "Chloplast",
+    value: "bar",
+    selectedBackgroundColor: "#fbc531"
+  }
+];
+
+const options3 = [
+  {
+    label: <span>Partially permeable membrane</span>,
+    value: {
+      foo: true
+    },
+    selectedBackgroundColor: "#0097e6",
+  },
+  {
+    label: "Impermeable membrane",
+    value: "bar",
+    selectedBackgroundColor: "#fbc531"
+  }
+];
+
+const options4 = [
+  {
+    label: <span>Cellulose</span>,
+    value: {
+      foo: true
+    },
+    selectedBackgroundColor: "#0097e6",
+  },
+  {
+    label: "Mitochondria",
     value: "bar",
     selectedBackgroundColor: "#fbc531"
   }
@@ -23,20 +68,59 @@ const onChange = (newValue) => {
   console.log(newValue);
 };
 
-const initialSelectedIndex = options.findIndex(({ value }) => value === "bar");
+const initialSelectedIndex1 = options1.findIndex(({ value }) => value === "bar");
+const initialSelectedIndex2 = options2.findIndex(({ value }) => value === "bar");
+const initialSelectedIndex3 = options3.findIndex(({ value }) => value === "bar");
+const initialSelectedIndex4 = options4.findIndex(({ value }) => value === "bar");
 
 export default function App() {
   return (
-    <div className="Frame48096201">
-      <h1 className='pageHeader'>Seneca Learning</h1>
-      
+    <div className="">
+      <h1>An Animal Cell Contains</h1>
+
 
       <div className="">
-        <p style={{ height: "100px" }}>
+        <p style={{ height: "80px" }}>
           <SwitchSelector
             onChange={onChange}
-            options={options}
-            initialSelectedIndex={initialSelectedIndex}
+            options={options1}
+            initialSelectedIndex={initialSelectedIndex1}
+            backgroundColor={"#353b48"}
+            fontColor={"#f5f6fa"}
+          />
+        </p>
+      </div>
+
+      <div className="">
+        <p style={{ height: "80px" }}>
+          <SwitchSelector
+            onChange={onChange}
+            options={options2}
+            initialSelectedIndex={initialSelectedIndex2}
+            backgroundColor={"#353b48"}
+            fontColor={"#f5f6fa"}
+          />
+        </p>
+      </div>
+
+      <div className="">
+        <p style={{ height: "80px" }}>
+          <SwitchSelector
+            onChange={onChange}
+            options={options3}
+            initialSelectedIndex={initialSelectedIndex3}
+            backgroundColor={"#353b48"}
+            fontColor={"#f5f6fa"}
+          />
+        </p>
+      </div>
+
+      <div className="">
+        <p style={{ height: "80px" }}>
+          <SwitchSelector
+            onChange={onChange}
+            options={options4}
+            initialSelectedIndex={initialSelectedIndex4}
             backgroundColor={"#353b48"}
             fontColor={"#f5f6fa"}
           />
