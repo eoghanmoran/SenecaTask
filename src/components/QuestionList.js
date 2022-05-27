d0001daa1066c3a10d0f813c71ec233714bf383d
40203858 <emoran05@qub.ac.uk>
2022-05-27 14:16:41 +0100

    Added Question List

---
+++ src/components/QuestionList.js
@@ -0,0 +1,26 @@
+const Questions = [
+    {
+        question: 'Who is the greatest?',
+        answers: [
+            [{ answer: 'Tiger Woods', isCorrect: true }, { answer: 'Jack Nicklaus', isCorrect: false }],
+            [{ answer: 'Lewis Hamilton', isCorrect: false }, { answer: 'Max Verstappen', isCorrect: true }],
+            [{ answer: 'Novak Djokovic', isCorrect: true }, { answer: 'Roger Federer', isCorrect: false }],
+            [{ answer: 'Christiano Ronaldo', isCorrect: false }, { answer: 'Lionel Messi', isCorrect: true }]
+
+
+        ]
+    },
+    {
+        question: 'An animal cell contains?',
+        answers: [
+            [{ answer: 'Cell Wall', isCorrect: false }, { answer: 'Ribosomes', isCorrect: true }],
+            [{ answer: 'Cytoplasm', isCorrect: true }, { answer: 'Chloroplast', isCorrect: false }],
+            [{ answer: 'Permeable membrane', isCorrect: true }, { answer: 'Impermeable membrane', isCorrect: false }],
+            [{ answer: 'Cellulose', isCorrect: false }, { answer: 'Mitochondria', isCorrect: true }]
+
+
+        ]
+    }
+
+]
+export default Questions;
